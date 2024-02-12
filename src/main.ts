64712cff8c12bbd40c18b506eca0da556a6d5edb
Amir KNANI <amir.knani@drone-geofencing.com>
2024-02-12 13:50:40 +0100

    👌 - feat: Adding config to run project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { Logger, ValidationPipe } from "@nestjs/common";
+import { json, urlencoded } from "express";
+import { IoAdapter } from "@nestjs/platform-socket.io";
 
+const logger: Logger = new Logger("Main");
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+
+	logger.log(AppModule);
+	app.useGlobalPipes(new ValidationPipe());
+	app.use(json({ limit: "50mb" }));
+	app.use(urlencoded({ extended: true, limit: "50mb" }));
+	app.useWebSocketAdapter(new IoAdapter(app));
+
+	await app.listen("3000");
+	logger.log("Application listening on port 3000");
 }
 bootstrap();
